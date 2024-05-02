4a18bd6fa2e269d1ac585703ba852bb24ffc0712
Kazakova Anastasia <kutianina.n@yandex.ru>
2024-05-02 14:00:22 +0500

    Uncomment script in module_7

--- module_7.js
+++ module_7.js
@@ -1,61 +1,57 @@
-// // Task 1
-// let word = prompt("Введите слово:");
-// let wordRevers = "";
+// Task 1
+let word = prompt("Введите слово:");
+let wordRevers = "";
 
-// if (word === ""){
-//     console.log("Введите слово")
-// } else {
-//     wordLower = word.toLowerCase();
-//     for (let i = 1; i <= wordLower.length; i++) {
-//         wordRevers += wordLower[wordLower.length - i];
-//     }
+if (word === ""){
+    console.log("Введите слово")
+} else {
+    wordLower = word.toLowerCase();
+    for (let i = 1; i <= wordLower.length; i++) {
+        wordRevers += wordLower[wordLower.length - i];
+    }
 
-//     if (wordLower === wordRevers) {
-//     console.log(`Слово ${word} является палиндромом`);
-//     } else {
-//     console.log(`Слово ${word} не является палиндромом`);
-//     }
-// }
+    if (wordLower === wordRevers) {
+    console.log(`Слово ${word} является палиндромом`);
+    } else {
+    console.log(`Слово ${word} не является палиндромом`);
+    }
+}
 
-// // Task 2
-// const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
-// const uniqueSet = new Set(arr);
-// const uniqueArray = Array.from(uniqueSet);
+// Task 2
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
+const uniqueSet = new Set(arr);
+const uniqueArray = Array.from(uniqueSet);
 
-// console.log(uniqueArray);
+console.log(uniqueArray);
 
-// // Task 3
-// let userInput = +prompt("Введите любое число:");
-// const arr = [];
+// Task 3
+let userInput = +prompt("Введите любое число:");
+const array = [];
 
-// for (let i = 0; i <= userInput; i++) {
-//     arr.push(i);
-// }
+for (let i = 0; i <= userInput; i++) {
+    array.push(i);
+}
 
-// console.log(arr);
+console.log(array);
 
 
-// //Task 4
-// const boardSize = 3;
-// const board = [];
+//Task 4
+const boardSize = 3;
+const board = [];
 
-// for (let i = 0; i < boardSize; i++) {
-//     const row = [];
-//     for (let j = 0; j < boardSize; j++) {
-//         const random = Math.random();
-//         const cellValue = random < 0.5 ? 'X' : 'O';
-//         row.push(cellValue);
-//     }
-//     board.push(row);
-// }
+for (let i = 0; i < boardSize; i++) {
+    const row = [];
+    for (let j = 0; j < boardSize; j++) {
+        const random = Math.random();
+        const cellValue = random < 0.5 ? 'X' : 'O';
+        row.push(cellValue);
+    }
+    board.push(row);
+}
 
-// for (let i = 0; i < boardSize; i++) {
-//     console.log(board[i].join(' '));
-// }
-
-// for (let i = 0; i < 3; i++) {
-//     console.log("Message");
-// }
+for (let i = 0; i < boardSize; i++) {
+    console.log(board[i].join(' '));
+}
 
 // Task 5
 const obj = {
